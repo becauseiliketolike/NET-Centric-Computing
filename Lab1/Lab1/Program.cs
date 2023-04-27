eaab0869f11be2c8642f9e1801374225df836ab1
unknown <norden.tamang@student.trinity.edu.np>
2023-04-27 16:50:59 +0545

    Lab 1 program added

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Employee
+    {
+        public string firstName, lastName;
+        public double salary;
+
+        public Employee(string firstName, string lastName, double salary) 
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.salary = salary;
+        }
+
+        public string FirstName
+        {
+            get { return firstName; } 
+            set {  firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public string FullName
+        {
+            get { return firstName + " " + lastName; }
+        }
+
+        public double Salary
+        {
+            get { return salary; }
+            set { salary = value;  }
+        }
+
+        public void IncrementSalary(double s) 
+        { 
+            salary += (s/100) * salary;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Fullname is: " + FullName + "\nSalary is: " + salary);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Radhe = new Employee("Radhe", "Futsal", 20000.0);
+            Console.WriteLine("Full Name: " + Radhe.FullName);
+            Console.WriteLine("Salary: " + Radhe.Salary);
+            Radhe.IncrementSalary(10);
+            Console.WriteLine("Salary: " + Radhe.Salary);
+            Radhe.Display();
+        }
+    }
+}
