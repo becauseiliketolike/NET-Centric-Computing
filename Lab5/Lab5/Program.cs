50fab5dedd9dbd59afe550d7821265978ff99327
becauseiliketolike <131786997+becauseiliketolike@users.noreply.github.com>
2023-05-19 09:58:01 +0545

    solved problem and update two versions

--- Lab5/Lab5/Program.cs
+++ Lab5/Lab5/Program.cs
@@ -12,21 +12,28 @@ Create table Users(id, usename, password, email, role, status)
 using System;
 using System.Data.SqlClient;
 
-
 namespace DatabaseConnection
-{
+{ 
     internal class InsertData
     {
         public static void Main(string[] args)
         {
-            var conn = "Data Source=(localdb)\\ProjectModels;Initial Catalog=myTest;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
-            SqlConnection db = new SqlConnection(conn);
-            var query = "Insert into dbo.Users values ('admin', 'admin', 'admin@admin.com', 'admin', 1)";
-            SqlCommand cmd = new SqlCommand(query, db);
+            var conn = "Data Source=(localdb)\\ProjectModels;Initial Catalog=myTest;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            var db = new SqlConnection(conn);
             db.Open();
+            var query = "Insert INTO dbo.Users(username, password, email, role, status) VALUES ('admin', 'admin', 'admin@gmail.com', 'admin', 1)";
+            SqlCommand cmd = new SqlCommand(query, db);
             cmd.ExecuteNonQuery();
             db.Close();
 
+            // ---------- Version:-2 ----------
+            // var conn = "Data Source=(localdb)\\ProjectModels;Initial Catalog=myTest;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            // SqlConnection db = new SqlConnection(conn);
+            // var query = "Insert into dbo.Users values('because', 'i', 'like@to.com', 'like', 2)";
+            // SqlCommand cmd = new SqlCommand(query, db);
+            // db.Open();
+            // cmd.ExecuteNonQuery();
+            // db.Close();
         }
     }
 }
