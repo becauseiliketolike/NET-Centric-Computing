a398c208c31910ff910efff3bbfeb3ee52acdc5f
unknown <norden.tamang@student.trinity.edu.np>
2023-06-08 16:00:33 +0545

    Lab 7 passing data from controller to view by veiwdata

---
+++ Lab7/Lab7/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab7.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(String name, int numTimes = 10) 
+        {
+            ViewData["Message"] =  "Hello" + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+
+        public IActionResult Google() {
+            return Redirect("https://www.google.com/");
+        }
+    }
+}
