cfe973ce000c87c8eebb8724ac7ff05ad73681a7
unknown <norden.tamang@student.trinity.edu.np>
2023-06-11 16:59:00 +0545

    lab 8 added

---
+++ Lab8/Lab8/Models/Register.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab8.Models
+{
+    public class Register
+    {
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? Email { get; set; }
+        public int Phone { get; set; }  
+    }
+}
