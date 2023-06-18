300482ad8f6acd893649ffea86a4eef4209fbe2d
unknown <norden.tamang@student.trinity.edu.np>
2023-06-18 16:05:48 +0545

    lab10 added

---
+++ Lab10/Lab10/Models/WebUser.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab10.Models
+{
+    public class WebUser
+    {
+        [Column("UserID")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserID { get; set; }
+        [Display(Name = "First Name")]
+        public string? FirstName { get; set; }
+        [Required]
+        [StringLength(25), MinLength(3)]
+        [Display(Name = "Last Name")]
+        public string? LastName { get; set; }
+        [EmailAddress]
+        public string? Email { get; set; }
+    }
+}
---
+++ Lab10/Lab10/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Lab10.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Lab10
+{
+    public class Startup
+    {
+        public IConfiguration configRoot
+        {
+            get;
+        }
+        public Startup(IConfiguration configuration)
+        {
+            configRoot = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddControllersWithViews();
+            services.AddDbContext<AppDbContext>(o => o.UseSqlServer(Configuration.GetConnectionString("DBConnectionString")));
+        }
+        public void Configure(WebApplication app, IWebHostEnvironment env, AppDbContext db)
+        {
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            db.Database.EnsureCreated();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.MapRazorPages();
+            app.Run();
+        }
+    }
+}
