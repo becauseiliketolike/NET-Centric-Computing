631e142f635eaddc5a933cc002233f95b172cfe5
becauseiliketolike <norden.tamang@student.trinity.edu.np>
2023-06-24 19:24:40 -0700

    Lab2 Assignments added

---
+++ Lab Assignments/Lab2/Lab2b/Lab2b/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2b.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Account()
+        {
+            return View();
+        }
+    }
+}
