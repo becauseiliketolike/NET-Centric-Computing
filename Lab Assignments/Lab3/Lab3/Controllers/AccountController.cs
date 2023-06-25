c5d3bfeee8e61b314cf00fa6f507a10e4f25f9ed
becauseiliketolike <norden.tamang@student.trinity.edu.np>
2023-06-25 07:30:17 -0700

    MVC app

---
+++ Lab Assignments/Lab3/Lab3/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab3.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Account()
+        {
+            return View();
+        }
+    }
+}
