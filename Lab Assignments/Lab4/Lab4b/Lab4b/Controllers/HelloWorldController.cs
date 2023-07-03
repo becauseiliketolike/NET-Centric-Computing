3bb0b01ef8ffed5a0912adf0969667b4979494d8
becauseiliketolike <norden.tamang@student.trinity.edu.np>
2023-07-02 21:34:44 -0700

    Passing data from controller to view using viewdata

---
+++ Lab Assignments/Lab4/Lab4b/Lab4b/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab4b.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Welcome()
+        {
+            ViewData["Message"] = "Hello Norden";
+            ViewData["NumTimes"] = 10;
+            return View();
+        }
+        public IActionResult Google()
+        {
+            return Redirect("https://www.google.com/");
+        }
+    }
+}
---
+++ Lab Assignments/Lab4/Lab4b/Lab4b/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lab4b.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
