0b90ae0b7e3eec3c5b2941f92b5d56834cc03786
becauseiliketolike <norden.tamang@student.trinity.edu.np>
2023-07-02 21:29:38 -0700

    Revert "Passing data from controller to view using viewdata"
    This reverts commit 6e2e9e4f23bcc93c7d2b633331b060d80601580d.

--- Lab Assignments/Lab4/Lab4b/Lab4b/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Lab4b.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
